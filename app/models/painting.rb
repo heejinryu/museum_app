c4bc22cbe362575f2c5dd6e90145489909f15ad4
Heejin Ryu <heejin.ryu@gmail.com>
2015-09-17 20:10:03 -0400

    Add paintings/index view, add search to paintings model

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,4 +1,10 @@
 class Painting < ActiveRecord::Base
 	belongs_to :museum
 	belongs_to :artist
+
+# we need class method since Search applies to all paintings; 
+# self. ==> class method, without self. ==> instance method
+	def self.search(query)
+		where('title LIKE :query', :query => "%#{query}%")
+	end
 end
